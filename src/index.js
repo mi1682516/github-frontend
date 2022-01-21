85001f91db3db4fa84c69ef4e8778bc148b4664e
mi7328224 <mi7328224@gmail.com>
2022-01-22 04:04:30 +0700

    redux test

--- src/index.js
+++ src/index.js
@@ -2,20 +2,16 @@ import App from './App'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Router from 'react-router-dom'
-import configureStore from './redux/configureStore'
+import store from './redux/configureStore'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'reduxjs-toolkit-persist'
 import { persistStore } from 'reduxjs-toolkit-persist'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={configureStore}>
-      <PersistGate loading ={null} persistor={persistStore(configureStore)}>
-        <Router.BrowserRouter>
-          <App />
-        </Router.BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <Router.BrowserRouter>
+      <App />
+    </Router.BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
