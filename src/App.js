1a447860bacae8887f6790fd5bd22f027fd14032
mi7328224 <mi7328224@gmail.com>
2022-01-22 06:55:57 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import * as Router from 'react-router-dom';
-import store from './redux/configureStore'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'reduxjs-toolkit-persist'
-import { persistStore } from 'reduxjs-toolkit-persist'
+
 
 
 function Home(){
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import App from './App'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Router from 'react-router-dom'
+import store from './redux/configureStore'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'reduxjs-toolkit-persist'
+import { persistStore } from 'reduxjs-toolkit-persist'
 
 ReactDOM.render(
   <React.StrictMode>
