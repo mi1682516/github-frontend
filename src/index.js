ce382bf6050ff7249f530d83eb134013c48f6741
mi7328224 <mi7328224@gmail.com>
2022-01-22 01:40:41 +0700

    redux test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import App from './App'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Router from 'react-router-dom'
-import configureStore from './redux/redux'
+import configureStore from './redux/configureStore'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'reduxjs-toolkit-persist'
 import { persistStore } from 'reduxjs-toolkit-persist'
