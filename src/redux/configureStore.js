b343e1c17f170721208eca9a0fad59d214a95f19
mi7328224 <mi7328224@gmail.com>
2022-01-22 16:13:51 +0700

    test

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,1 @@
-import {persistReducer} from 'reduxjs-toolkit-persist'
-import {combineReducers,configureStore} from '@reduxjs/toolkit';
-import storage from 'reduxjs-toolkit-persist/lib/storage'
-
-import counter from './reducers/counter'
-
-const reducers = combineReducers({
-  counter : counter
-})
-
-const config = {key : 'root',storage}
-
-export default configureStore({
-  reducer : persistReducer(
-    config,reducers
-  )
-})
+import {createStore} from 'redux'
