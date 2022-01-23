2462b2b982dff9299320087fbb37db76fca95d45
mi7328224 <mi7328224@gmail.com>
2022-01-23 08:03:38 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import store from './redux/store';
-import {Provider} from 'react-redux';
-import {persistStore} from 'redux-persist';
-import {PersistGate} from 'redux-persist/integration/react';
 
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistStore(store)}>
-        <div className="App">
-          <h2>Hello</h2>
-        </div>
-      </PersistGate>
-    </Provider>
+    <div className="App">
+      <h2>Hello</h2>
+    </div>
   )
   
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import App from './App'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './redux/store';
+import {Provider} from 'react-redux';
+import {persistStore} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
